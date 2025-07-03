0b6227b99241b361a95b06512ea0c5c77e0ef0f6
Guilherme <gfmolon@gmail.com>
2025-07-03 11:42:28 -0300

    Create bubble_sort_vetor.c

---
+++ bubble_sort_vetor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int v[5];
+    int i, j, aux;
+
+    for(i=0; i<5; i++) {
+        printf("Numero\n");
+        scanf("%d",&v[i]);
+    }
+
+    for(i=0; i<5; i++) {
+        for(j=0; j<5-i; j++) {
+            if( v[j] > v[j+1]) {
+                aux = v[j];
+                v[j] = v[j+1];
+                v[j+1] = aux;
+            }
+        }
+    }
+
+    for(i=0; i<5; i++) {
+        printf("%d\n",v[i]);
+    }
+
+
+
+
+
+    return 0;
+
+}
