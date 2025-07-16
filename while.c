91f526516be44270a5dcf799c3805afee2acb3da
Guilherme <gfmolon@gmail.com>
2025-07-16 17:18:09 -0300

    Create while.c

---
+++ while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    i = 0;
+    while(1) {
+        printf("POOOwwWWWW!!! %d\n", i);
+        i++;
+
+        if (i==50) {
+            break;
+        }
+    }
+
+    printf("Programa Encerrado.\n");
+
+}
