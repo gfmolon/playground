9cec45f43c34c6dcf950b48c8a2789ae7115db65
Guilherme <gfmolon@gmail.com>
2025-09-22 09:09:42 -0300

    Create treinando_conceitos.c

---
+++ treinando_conceitos.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, n2, achou, i, j, aux, limite, k, dados[5], procurar;
+
+    k=0;
+    limite = 0;
+    while(limite < 5) { 
+        printf("Informe um valor:");
+        scanf("%d",&n);
+
+        if (n < 0) {
+            break;
+        }
+        dados[k] = n;
+        k++;
+        limite++;
+    }
+
+    printf("Dados inseridos:\n");
+    for(i=0; i<k; i++) {
+        printf("%d\n", dados[i]);
+    }
+
+    printf("Lista Ordenada:\n");
+    for(i=0; i<k; i++) {
+        for(j=0; j < k-1-i; j++) {
+            if (dados[j] > dados[j+1]) {
+                aux = dados[j];
+                dados[j] = dados[j+1];
+                dados[j+1] = aux;
+            }
+        }
+    } 
+
+    for(i=0; i<k; i++) {
+        printf("%d\n",dados[i]);
+    }
+
+    printf("Procurar no vetor? (1/0)");
+    scanf("%d",&procurar);
+   
+    achou = 0;
+    if (procurar == 1) {
+        printf("Informe um número que deseja procurar:\n");
+        scanf("%d",&n2);
+
+        for (i=0; i<k; i++) {
+            if (dados[i] == n2) {
+                achou++;
+                break;
+            }
+        }
+
+        if (achou) {
+            printf("%d está na lista!\n", n2);
+        }
+        else {
+            printf("Este número não está na lista.\n");
+        }
+
+    }
+
+
+    return 0;
+}
